a3f5e822325613da0de5d0da124eb412b3680ec2
Juan I. Lanosa <blackmoodgames@gmail.com>
2020-11-26 01:54:52 -0300

    estupendo todo

---
+++ Assets/RegulateColorBasedOnPitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegulateColorBasedOnPitch : MonoBehaviour
+{
+    public AudioSource audio;
+
+    [Header("Type (opcional). Cargar sobre objeto img o sp.rend")]
+    public UnityEngine.UI.Image img;
+    public SpriteRenderer sprR;
+
+    public Color colorBackwards = Color.black;
+    private Color firstColor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent<UnityEngine.UI.Image>(out img);
+        TryGetComponent<SpriteRenderer>(out sprR);
+        
+        if (sprR)
+            firstColor = sprR.color;
+        else
+            firstColor = img.color;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (sprR)
+            sprR.color = firstColor;
+        else
+            img.color = firstColor;
+
+
+        if (audio.pitch > 0.56f)
+            return;
+
+
+        if (sprR)
+            sprR.color = colorBackwards;
+        else
+            img.color = colorBackwards;
+    }
+}
