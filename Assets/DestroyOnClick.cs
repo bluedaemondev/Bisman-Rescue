8c2ec2b902ab7e031919ed17b40fc22718c2f94f
Juan I. Lanosa <blackmoodgames@gmail.com>
2020-12-05 21:43:47 -0300

    story prototype

---
+++ Assets/DestroyOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+        if(Input.anyKeyDown && Time.timeSinceLevelLoad >= 1f)
+            Destroy(this.gameObject);
+    }
+}
